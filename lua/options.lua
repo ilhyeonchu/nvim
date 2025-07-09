a712b16446dca8a62279ae5222532c08c765e80f
chooilhyeon <168050490+chooilhyeon@users.noreply.github.com>
2025-07-09 18:01:49 +0900

    파이썬 루아 줄리아 등 주석 및 스마ㅡ인댄트설정

--- lua/options.lua
+++ lua/options.lua
@@ -8,13 +8,21 @@ vim.opt.autoindent = true
 vim.opt.smartindent = false
 vim.opt.cindent = false
 
--- C,C++,Java 등 C 계열 파일만 smartindent 활성화
+-- C,C++,Go,Java 등 C 계열 파일만 smartindent 활성화
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"c", "cpp", "java"},
-  callback = function()
-    vim.bo.smartindent = true
-    vim.opt_local.formatoptions:remove({ "c", "r", "o"})
-  end,
+    pattern = {"c", "cpp", "java", "go"},
+    callback = function()
+        vim.bo.smartindent = true
+        vim.opt_local.formatoptions:remove({ "c", "r", "o"})
+    end,
+})
+-- 파이썬 등 smartindent 비활성화
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"python", "lua", "julia"},
+    callback = function()
+        vim.bo.smartindent = false
+        vim.opt_local.formatoptions:remove({"c", "r", "o"})
+    end,
 })
 
 -- 줄 번호 표시 등 추가 옵션 (init_numbering.lua 참고)
